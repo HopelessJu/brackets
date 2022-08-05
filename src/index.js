79bde26ff5b879b695efdceafeb67d77965a0653
Aleksandra <s.tsikhmanovich@gmail.com>
2022-08-05 12:08:46 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let baseStr = str.length;
+  let newStr = '';
+
+  while (newStr !== baseStr) {
+    baseStr = str.length;
+    bracketsConfig.forEach(pairs => {pairs = pairs.join('')
+      while(str.indexOf(pairs) >= 0) {
+      str = str.replace(pairs, '');
+      }
+  });
+
+    newStr = str.length;
+  }
+
+  return newStr == 0 ? true : false;
 }
